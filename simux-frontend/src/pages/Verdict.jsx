ee087b4e8b8f1ef0ac2fdb13640b2aa77969b1a5
tfm4n <noenoeke11@gmail.com>
2026-05-20 08:51:45 +0000

    ui: refine Verdict.jsx

--- simux-frontend/src/pages/Verdict.jsx
+++ simux-frontend/src/pages/Verdict.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import axios from "axios";
 
-const API = "http://localhost:3000";
+const API = import.meta.env.VITE_API_URL
 
 const VERDICT_MAP = {
   AC: {
